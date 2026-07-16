023557bcab9e7f972676855ebda8fc0172395bbc
Shirkan84 <shir.kanevsky@gmail.com>
2026-07-16 11:39:29 +0300

    Add Supabase authentication

---
+++ src/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ytdkpbuzycvspexnkeci.supabase.co'
+const supabasePublishableKey = 'sb_publishable_Zu5t4GsdcE3Kbw2Ztt-PwQ_yNoH3yvQ'
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey)
