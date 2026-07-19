e51129da8f58c74679e051dabeae73aa6bc19c7a
Shirkan84 <shir.kanevsky@gmail.com>
2026-07-19 16:41:56 +0300

    Export Supabase client config

--- src/supabase.ts
+++ src/supabase.ts
@@ -4,3 +4,4 @@ const supabaseUrl = 'https://ytdkpbuzycvspexnkeci.supabase.co'
 const supabasePublishableKey = 'sb_publishable_Zu5t4GsdcE3Kbw2Ztt-PwQ_yNoH3yvQ'
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey)
+export { supabaseUrl, supabasePublishableKey }
